c43cf37f947a2352b9fa444fdd215bd7e027fa0e
Y. Forku <70104051+cbkadal@users.noreply.github.com>
2024-02-15 13:31:48 +0700

    modified:   Makefile modified:   djbx33a.c modified:   djbx33a.sh

---
+++ yforku/W01/djbx33a.c
@@ -0,0 +1,14 @@
+/* 
+ * Copyright (C) 2024-2024 Y. Forku
+ * This free software is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * START: Thu 15 Feb 2024 13:00
+ */
+
+#include <stdio.h>
+
+int main(void) {
+   printf("Hello\n");
+}
+
