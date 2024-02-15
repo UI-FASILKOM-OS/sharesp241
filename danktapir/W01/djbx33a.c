e9211620162a11d24a3f0c854a5ddadd429470c0
Rasyad Jabbar Poncosewoyo <89518333+danktapir@users.noreply.github.com>
2024-02-15 15:16:37 +0700

    Fixed different hashed values

--- danktapir/W01/djbx33a.c
+++ danktapir/W01/djbx33a.c
@@ -1,19 +1,21 @@
+#include <stdio.h>
 #include <stdint.h>
 
-uint32_t djb2_hash(const char *str) {
-  uint32_t hash = 5381;
-  int c;
+uint32_t djbx33a_hash(const char* str) {
+    uint32_t hash = 5381;
+    int c;
 
-  while ((c = *str++)) {
-    hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-  }
+    while ((c = *str++)) {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
 
-  return hash;
+    return hash;
 }
 
 int main() {
-	const char *input = "The DJBX33A result with C and Bash should be the same";
-	uint32_t hash_result = djb2_hash(input);
-	printf("Hash value using C: %u\n", hash_result);
-	return 0;
+    const char* test_string = "The DJBX33A result with C and Bash should be the same";
+    uint32_t hash_value = djbx33a_hash(test_string);
+    printf("Hash value of '%s' using DJBX33A: %u\n", test_string, hash_value);
+    return 0;
 }
+
--- danktapir/W01/djbx33a.sh
+++ danktapir/W01/djbx33a.sh
@@ -1,14 +1,9 @@
-function djb2_hash {
-  hash=5381
-  for char in "${1// }"; do # Loop through each character, excluding spaces
-    hash=$(( (hash * 33) + char ))
-  done
-  echo "$hash"
-}
-
-# Test the function
-test_string="The DJBX33A result with C and Bash should be the same"
-hash_value=$(djb2_hash "$test_string")
-echo "Hash value of '$test_string' using DJBX33A: $hash_value"
+hash=5381
+input="The DJBX33A result with C and Bash should be the same"
+for ((i=0; i<${#input}; i++)); do
+    char=${input:$i:1}
+    hash=$((hash * 33 + $(printf "%d" "'$char")))
+done
+echo DJBX33A hash result: $((hash & 0x7FFFFFFF))
 
 exit
