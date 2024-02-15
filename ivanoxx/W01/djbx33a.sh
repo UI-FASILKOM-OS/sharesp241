e69c7248735bd27d90db8dbb38f18648d074df07
Dafi Nafidz <65144204+ivanoxx@users.noreply.github.com>
2024-02-15 13:46:43 +0700

    Update djbx33a.sh

--- ivanoxx/W01/djbx33a.sh
+++ ivanoxx/W01/djbx33a.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+
+# Function to calculate the DJBX33A hash
+djb2() {
+    str=$1
+    hash=5381
+
+    for (( i=0; i<${#str}; i++ )); do
+        c=$(printf "%d" "'${str:$i:1}")
+        hash=$(( (hash << 5) + hash + c )) # hash * 33 + c
+    done
+
+    printf "%x\n" $hash
+}
+
+# Main script
+str="The DJBX33A result with C and Bash should be the same"
+hash=$(djb2 "$str")
+echo "Hash: $hash"
 
