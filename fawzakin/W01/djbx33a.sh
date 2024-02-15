27b228aa0d1cc1c02df36ee2d1c3476bfd7ec03f
fawzakin <8430350-fawzakin@users.noreply.gitlab.com>
2024-02-15 14:09:29 +0700

    Attempt dbjx33a.sh for fawzakin

--- fawzakin/W01/djbx33a.sh
+++ fawzakin/W01/djbx33a.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
-echo "The DJBX33A result with C and Bash should be the same"
+#!/usr/bin/env bash
+text="The DJBX33A result with C and Bash should be the same"
+hash_num=5381
+for (( i=0; i<${#text}; i++ )); do
+  charr="${text:$i:1}"
+  ascii=$(printf "%d" "'$charr")
+  hash_num=$((hash_num*33+ascii))
+done
+printf "Hash value of '%s' using DJBX33A is %s" "$text" "$hash_num" 
