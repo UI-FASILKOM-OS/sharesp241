57015781f89cc0729be2ff406452f1b6bf0f65c0
Cicak Bin Kadal <70104051+cbkadal@users.noreply.github.com>
2024-02-29 13:38:41 +0700

    new file:   cbkadal/W03/cbkadal/Makefile new file:   cbkadal/W03/cbkadal/cbkadal.c modified:   cbkadal/W03/progress.txt

---
+++ cbkadal/W03/cbkadal/cbkadal.c
@@ -0,0 +1,16 @@
+/* 
+ * Copyright (C) 2016-2024 C. BinKadal Sdn. Bhd.
+ * This free software is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * REV09: Thu 29 Feb 2024 13:00
+ * REV08: Sun 06 Aug 2023 12:00
+ * START: Fri 01 Jan 2016 00:00
+ */
+
+#include <stdio.h>
+
+int main(void) {
+   printf("Hello WEEK03!\n");
+}
+
