1ea655cc0128c8c821c4a042b2b1d6985212ef78
ivanoxx <65144204+ivanoxx@users.noreply.github.com>
2024-03-12 22:39:02 +0700

    done

---
+++ ivanoxx/W04/2106701564-getopt.c
@@ -0,0 +1,67 @@
+/*************************************************************************\
+*                  Copyright (C) Michael Kerrisk, 2022.                   *
+*                                                                         *
+* This program is free software. You may use, modify, and redistribute it *
+* under the terms of the GNU General Public License as published by the   *
+* Free Software Foundation, either version 3 or (at your option) any      *
+* later version. This program is distributed without any warranty.  See   *
+* the file COPYING.gpl-v3 for details.                                    *
+* CBKADAL: Modified for ASSIGNMENT WEEK05                                 *
+\*************************************************************************/
+
+// Listing B-1
+// t_getopt.c: Demonstrate the use of getopt(3) to parse command-line options.
+
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+#ifdef __GNUC__
+__attribute__((noreturn))
+#endif
+
+static void             /* Print "usage" message and exit */
+usageError(void)
+{
+    fprintf(stderr, "ERROR\n");
+    exit(EXIT_FAILURE);
+}
+
+
+int
+main(int argc, char *argv[])
+{
+    int opt, bfnd;
+    char *pstr;
+	bool world = false;
+
+    bfnd = 0;
+    pstr = NULL;
+
+    while ((opt = getopt(argc, argv, "bn:")) != -1) {
+        switch (opt) {
+        case 'n': pstr = optarg;        break;
+        case 'b': bfnd++;               break;
+        case ':': usageError();
+        case '?': usageError();
+        default:  usageError();
+        }
+    }
+	for (int i; i < argc; i++) {
+		if (!strcmp(argv[i], "World")) {
+			world = true;
+			puts("Hello World!");
+		}
+	}
+
+	if (!world)
+		puts("Hello!");
+	if (bfnd != 0)
+		puts("It is a beautiful day!");
+    if (pstr != NULL)
+        printf("Is your name %s?\n", pstr);
+    exit(EXIT_SUCCESS);
+}
+
