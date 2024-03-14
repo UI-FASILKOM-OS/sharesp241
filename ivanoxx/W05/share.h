f4fbabb98c3af14598b83df86faae17e5e772424
ivanoxx <65144204+ivanoxx@users.noreply.github.com>
2024-03-14 14:53:28 +0700

    progress

--- ivanoxx/W05/share.h
+++ ivanoxx/W05/share.h
@@ -1,11 +1,11 @@
-// REV03: Wed 14 Mar 2024 13:38
+// REV04: Wed 14 Mar 2024 14:41
 // START: Sat 11 Mar 2023 22:00
 // Please complete this file.
 
 #ifndef SHARE_H
 #define SHARE_H
 
-void dummy();
+extern void dummy();
 
 #define GITHUB_ACCOUNT "ivanoxx"
 
