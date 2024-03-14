1b4cea33360f17742b1db605dd61f868bdb37b7e
mm0ne <87106378+mm0ne@users.noreply.github.com>
2024-03-14 13:43:14 +0700

    progress

---
+++ mm0ne/W05/share.h
@@ -0,0 +1,12 @@
+// REV02: Wed 13 Mar 2024 10:00
+// START: Sat 11 Mar 2023 22:00
+// Please complete this file.
+
+#ifndef SHARE_H
+#define SHARE_H
+
+extern void dummy();
+#define GITHUB_ACCOUNT "mm0ne"
+
+#endif /* SHARE_H */
+
