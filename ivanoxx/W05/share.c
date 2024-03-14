e052d036a9916e726c96a94ec8fccf306293a97e
ivanoxx <65144204+ivanoxx@users.noreply.github.com>
2024-03-14 13:33:13 +0700

    progress

---
+++ ivanoxx/W05/share.c
@@ -0,0 +1,14 @@
+// REV03: Thu 14 Mar 2024 13:26:45 WIB
+// START: Sat 11 Mar 2023 22:00
+// Please complete this file.
+
+#include <stdio.h>
+
+void dummy() {
+        puts("ivanoxx");
+}
+
+int main() {
+        dummy();
+        return 0;
+}
