143f0fd0a745218b4d7c64f9cbbed0f296a5a7cc
KingGedorah <naufal.zhafari@ui.ac.id>
2024-03-20 15:30:04 +0700

    update progress

---
+++ KingGedorah/W05/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+autoreconf --install
+./configure
+make
+./prog1
+./prog2
---
+++ KingGedorah/W05/share.c
@@ -0,0 +1,11 @@
+// REV02: Wed 13 Mar 2024 10:00
+// START: Sat 11 Mar 2023 22:00
+// Please complete this file.
+
+
+#include <stdio.h>
+#include "share.h"
+
+void dummy() {
+	printf("%s\n", GITHUB_ACCOUNT);
+}
