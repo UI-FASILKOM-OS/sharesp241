7c08a753b9135a1ccaae1bf60c79e6f5a1483e01
Cicak Bin Kadal <70104051+cbkadal@users.noreply.github.com>
2024-03-20 22:11:08 +0700

    modified:   cbkadal/W05/build.sh

--- cbkadal/W05/build.sh
+++ cbkadal/W05/build.sh
@@ -6,21 +6,22 @@ aclocal
 autoconf
 automake --add-missing
 ./configure
+sleep 2; echo "make ==== =========="
 make
-sleep 2; echo "prog1 ===="
+sleep 2; echo "prog1 ==== =========="
 ./prog1
-sleep 2; echo "prog2 ===="
+sleep 2; echo "prog2 ==== =========="
 ./prog2
-sleep 2; echo "touch prog1.c ===="
+sleep 2; echo "touch prog1.c ==== =========="
 touch prog1.c
 make
-sleep 2; echo "touch prog2.c ===="
+sleep 2; echo "touch prog2.c ==== =========="
 touch prog2.c
 make
-sleep 2; echo "touch share.c ===="
+sleep 2; echo "touch share.c ==== =========="
 touch share.c
 make
-sleep 2; echo "touch share.h ===="
+sleep 2; echo "touch share.h ==== =========="
 touch share.h
 make
 
